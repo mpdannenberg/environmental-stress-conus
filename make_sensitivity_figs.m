81c59e681d3dbefd02f2bab28d7f22537e3269af
mpdannenberg <matthew-dannenberg@uiowa.edu>
2019-07-22 17:44:10 -0500

    Delete obsolete commented-out code

--- make_sensitivity_figs.m
+++ make_sensitivity_figs.m
@@ -18,23 +18,13 @@ plot(pipo(:, 2), pipo(:, 1), 'o', 'Color','k', 'LineWidth',...
 set(gca, 'XLim',[0 150], 'YLim',[0 150]);
 hold on;
 plot([0 150], [0 150], '-', 'Color',[0.4 0.4 0.4]);
-% plot(pifl(:, 2), pifl(:, 1), 'o', 'Color',[55,126,184]/255, 'LineWidth',...
-%     1.2, 'MarkerSize',5);
 plot(psme(:, 2), psme(:, 1), 'o', 'Color',[228,26,28]/255, 'LineWidth',...
     1.2, 'MarkerSize',5);
 [r, p] = corr(pipo, 'type','Spearman');
 text(5, 142, '\itPinus ponderosa', 'FontSize',11)
 text(5, 136, ['n = ', num2str(length(pipo(:, 2)))], 'FontSize',11)
 text(5, 130, ['\rho = ', num2str(round(r(1,2), 2))], 'FontSize',11)
-% [r, p] = corr(pifl, 'type','Spearman');
-% % text(30, 140, ['n = ', num2str(length(pifl(:, 2)))], 'FontSize',12, 'Color',[55,126,184]/255)
-% % text(30, 132, ['\rho = ', num2str(round(r(1,2), 2))], 'FontSize',12, 'Color',[55,126,184]/255)
-% text(5, 118, '\itPinus flexilis', 'FontSize',11, 'Color',[55,126,184]/255)
-% text(5, 112, ['n = ', num2str(length(pifl(:, 2)))], 'FontSize',11, 'Color',[55,126,184]/255)
-% text(5, 106, ['\rho = ', num2str(round(r(1,2), 2))], 'FontSize',11, 'Color',[55,126,184]/255)
 [r, p] = corr(psme, 'type','Spearman');
-% text(55, 140, ['n = ', num2str(length(psme(:, 2)))], 'FontSize',12, 'Color',[228,26,28]/255)
-% text(55, 132, ['\rho = ', num2str(round(r(1,2), 2))], 'FontSize',12, 'Color',[228,26,28]/255)
 text(5, 118, '\itPseudotsuga menziesii', 'FontSize',11, 'Color',[228,26,28]/255)
 text(5, 112, ['n = ', num2str(length(psme(:, 2)))], 'FontSize',11, 'Color',[228,26,28]/255)
 text(5, 106, ['\rho = ', num2str(round(r(1,2), 2))], 'FontSize',11, 'Color',[228,26,28]/255)
@@ -44,16 +34,9 @@ set(gca, 'Position',[0.1300    0.43    0.7750    0.5154]);
 text(-20, 150, '(a)', 'FontSize',14, 'FontWeight','bold');
 
 subplot(3, 1, 3)
-% [hst, edges] = histcounts(pipo(:, 1)-pipo(:, 2), -100:10:20, 'Normalization','probability');
-% plot(-95:10:15, hst, '-', 'LineWidth',1.2, 'Color','k');
 histogram(pipo(:, 1)-pipo(:, 2), -100:5:20, 'FaceAlpha',1, 'FaceColor','k', 'EdgeColor',[0.4 0.4 0.4]);
 set(gca, 'YLim',[0 60]);
 hold on;
-% [hst, edges] = histcounts(pifl(:, 1)-pifl(:, 2), -100:10:20, 'Normalization','probability');
-% plot(-95:10:15, hst, '-', 'LineWidth',1.2, 'Color',[55,126,184]/255);
-% histogram(pifl(:, 1)-pifl(:, 2), -100:5:20, 'FaceAlpha',1, 'FaceColor',[55,126,184]/255, 'EdgeColor',[0.4 0.4 0.4]);
-% [hst, edges] = histcounts(psme(:, 1)-psme(:, 2), -100:10:20, 'Normalization','probability');
-% plot(-95:10:15, hst, '-', 'LineWidth',1.2, 'Color',[228,26,28]/255);
 histogram(psme(:, 1)-psme(:, 2), -100:5:20, 'FaceAlpha',1, 'FaceColor',[228,26,28]/255, 'EdgeColor',[0.4 0.4 0.4]);
 xlabel('DBH bias (cm)', 'FontSize',12);
 ylabel('Count', 'FontSize',12);
@@ -73,12 +56,6 @@ fprintf(['Mean bias: ', num2str(mean(pipo(:, 1)-pipo(:, 2))),'\n']);
 fprintf(['Median bias: ', num2str(median(pipo(:, 1)-pipo(:, 2))),'\n']);
 fprintf('\n');
 
-% % PIFL
-% fprintf('PIFL:\n');
-% fprintf(['Mean bias: ', num2str(mean(pifl(:, 1)-pifl(:, 2))),'\n']);
-% fprintf(['Median bias: ', num2str(median(pifl(:, 1)-pifl(:, 2))),'\n']);
-% fprintf('\n');
-
 % PSME
 fprintf('PSME:\n');
 fprintf(['Mean bias: ', num2str(mean(psme(:, 1)-psme(:, 2))),'\n']);
@@ -91,13 +68,11 @@ nbins = 100; % Number of bins within which to calculate median and quantiles
 quants = [0.025 0.975];
 
 % 1-D*, 2-D, 3-Dopt*, 4-Dopt, 5-dD, 6-Dopt bias, 7-S*, 8-S, 9-S bias
-cd PIPO;
-pipo = csvread('Sensitivity_PIPO.csv', 1, 1);
-% cd ../PIFL;
-% pifl = csvread('Sensitivity_PIFL.csv', 1, 1);
-cd ../PSME;
-psme = csvread('Sensitivity_PSME.csv', 1, 1);
-cd ..;
+pipo = csvread('./data/Sensitivity_PIPO.csv', 1, 2);
+psme = csvread('./data/Sensitivity_PSME.csv', 1, 2);
+acru = csvread('./data/Sensitivity_ACRU.csv', 1, 2);
+quco = csvread('./data/Sensitivity_QUCO.csv', 1, 2);
+quve = csvread('./data/Sensitivity_QUVE.csv', 1, 2);
 
 % Plots: PIPO
 Dstar = pipo(:,1);
@@ -118,15 +93,6 @@ bins = (0+(0.5/nbins)):(1/nbins):(1-(0.5/nbins));
 bins = quantile(Dstar, bins);
 PIPO.Dopt.bins = bins;
 PIPO.S.bins = bins;
-
-% spl = fit(Dstar(~isnan(Dopt_bias)), Dopt_bias(~isnan(Dopt_bias)), 'smoothingspline');
-% yhat = feval(spl, 0:1:max(bins));
-% PIPO.Dopt.spl = yhat;
-% PIPO.Dopt.spl_bins = 0:1:max(bins);
-% spl = fit(Dstar(~isnan(S_bias)), S_bias(~isnan(S_bias)), 'smoothingspline');
-% yhat = feval(spl, 0:1:max(bins));
-% PIPO.S.spl = yhat;
-% PIPO.S.spl_bins = 0:1:max(bins);
 
 for i = 1:nbins
     
@@ -151,57 +117,6 @@ for i = 1:nbins
     
 end
 
-% % Plots: PIFL
-% Dstar = pifl(:,1);
-% Dopt_bias = pifl(:, 6);
-% S_bias = pifl(:, 9);
-% S_bias(~isfinite(S_bias)) = NaN;
-% 
-% PIFL.Dopt.y = NaN(1, nbins);
-% PIFL.Dopt.uci = NaN(1, nbins);
-% PIFL.Dopt.lci = NaN(1, nbins);
-% PIFL.S.y = NaN(1, nbins);
-% PIFL.S.uci = NaN(1, nbins);
-% PIFL.S.lci = NaN(1, nbins);
-% 
-% x = quantile(Dstar, 0:(1/nbins):1);
-% bins = (0+(0.5/nbins)):(1/nbins):(1-(0.5/nbins));
-% bins = quantile(Dstar, bins);
-% PIFL.Dopt.bins = bins;
-% PIFL.S.bins = bins;
-
-% spl = fit(Dstar(~isnan(Dopt_bias)), Dopt_bias(~isnan(Dopt_bias)), 'smoothingspline');
-% yhat = feval(spl, 0:1:max(bins));
-% PIFL.Dopt.spl = yhat;
-% PIFL.Dopt.spl_bins = 0:1:max(bins);
-% spl = fit(Dstar(~isnan(S_bias)), S_bias(~isnan(S_bias)), 'smoothingspline');
-% yhat = feval(spl, 0:1:max(bins));
-% PIFL.S.spl = yhat;
-% PIFL.S.spl_bins = 0:1:max(bins);
-
-% for i = 1:nbins
-%     
-%     if i==1
-%         idx = Dstar>=x(i) & Dstar<=x(i+1);
-%     else
-%         idx = Dstar>x(i) & Dstar<=x(i+1);
-%     end
-%     Dopt_sub = Dopt_bias(idx);
-%     S_sub = S_bias(idx);
-%     
-%     PIFL.Dopt.y(i) = median(Dopt_sub);
-%     PIFL.S.y(i) = median(S_sub);
-%     
-%     cis = quantile(Dopt_sub, quants);
-%     PIFL.Dopt.lci(i) = cis(1);
-%     PIFL.Dopt.uci(i) = cis(2);
-%     
-%     cis = quantile(S_sub, quants);
-%     PIFL.S.lci(i) = cis(1);
-%     PIFL.S.uci(i) = cis(2);
-%     
-% end
-
 % Plots: PSME
 Dstar = psme(:,1);
 Dopt_bias = psme(:, 6);
@@ -220,15 +135,6 @@ bins = (0+(0.5/nbins)):(1/nbins):(1-(0.5/nbins));
 bins = quantile(Dstar, bins);
 PSME.Dopt.bins = bins;
 PSME.S.bins = bins;
-
-% spl = fit(Dstar(~isnan(Dopt_bias)), Dopt_bias(~isnan(Dopt_bias)), 'smoothingspline');
-% yhat = feval(spl, 0:1:max(bins));
-% PSME.Dopt.spl = yhat;
-% PSME.Dopt.spl_bins = 0:1:max(bins);
-% spl = fit(Dstar(~isnan(S_bias)), S_bias(~isnan(S_bias)), 'smoothingspline');
-% yhat = feval(spl, 0:1:max(bins));
-% PSME.S.spl = yhat;
-% PSME.S.spl_bins = 0:1:max(bins);
 
 for i = 1:nbins
     
@@ -254,9 +160,6 @@ for i = 1:nbins
 end
 
 
-
-
-
 %% Dopt bias
 h = figure('Color','w');
 h.Units = 'inches';
@@ -270,19 +173,8 @@ set(gca, 'YLim',[-0.44 0.14],'XLim', [0 90], 'XTickLabels','');
 hold on;
 plot([0 90], [0 0], 'k-', 'LineWidth',0.3);
 plot(PIPO.Dopt.bins, PIPO.Dopt.y, 'k-','LineWidth',1.2);
-% plot(PIPO.Dopt.spl_bins, PIPO.Dopt.spl, 'k-','LineWidth',1.2);
 text(2, 0.09, '\itPinus ponderosa', 'FontSize',12, 'Color','k');
 ylabel('\DeltaD_{opt}^{*} bias (cm)');
-
-% axes(ax(2))
-% fill([PIFL.Dopt.bins fliplr(PIFL.Dopt.bins)], [PIFL.Dopt.lci fliplr(PIFL.Dopt.uci)], [55,126,184]/255, 'FaceAlpha',0.4, 'EdgeColor','none')
-% set(gca, 'YLim',[-0.44 0.14],'XLim', [0 90], 'XTickLabels','');
-% hold on;
-% plot([0 90], [0 0], 'k-', 'LineWidth',0.3);
-% plot(PIFL.Dopt.bins, PIFL.Dopt.y, '-','LineWidth',1.2, 'Color',[55,126,184]/255);
-% % plot(PIFL.Dopt.spl_bins, PIFL.Dopt.spl, '-','LineWidth',1.2, 'Color',[55,126,184]/255);
-% text(2, 0.09, '\itPinus flexilis', 'FontSize',12, 'Color',[55,126,184]/255);
-% ylabel('\DeltaD_{opt}^{*} bias (cm)');
 
 axes(ax(2))
 fill([PSME.Dopt.bins fliplr(PSME.Dopt.bins)], [PSME.Dopt.lci fliplr(PSME.Dopt.uci)], [228,26,28]/255, 'FaceAlpha',0.4, 'EdgeColor','none')
@@ -290,7 +182,6 @@ set(gca, 'YLim',[-0.44 0.14],'XLim', [0 90]);
 hold on;
 plot([0 90], [0 0], 'k-', 'LineWidth',0.3);
 plot(PSME.Dopt.bins, PSME.Dopt.y, '-','LineWidth',1.2, 'Color',[228,26,28]/255);
-% plot(PSME.Dopt.spl_bins, PSME.Dopt.spl, '-','LineWidth',1.2, 'Color',[228,26,28]/255);
 text(2, 0.09, '\itPseudotsuga menziesii', 'FontSize',12, 'Color',[228,26,28]/255);
 ylabel('\DeltaD_{opt}^{*} bias (cm)');
 xlabel('D^{*} (cm)');
@@ -313,19 +204,8 @@ set(gca, 'YLim',[-0.28 0.85],'XLim', [0 90], 'XTickLabels','');
 hold on;
 plot([0 90], [0 0], 'k-', 'LineWidth',0.3);
 plot(PIPO.S.bins, PIPO.S.y, 'k-','LineWidth',1.2);
-% plot(PIPO.S.spl_bins, PIPO.S.spl, 'k-','LineWidth',1.2);
 text(8, 0.76, '\itPinus ponderosa', 'FontSize',12, 'Color','k');
 ylabel('S^{*} bias');
-
-% axes(ax(2))
-% fill([PIFL.S.bins fliplr(PIFL.S.bins)], [PIFL.S.lci fliplr(PIFL.S.uci)], [55,126,184]/255, 'FaceAlpha',0.4, 'EdgeColor','none')
-% set(gca, 'YLim',[-0.28 0.85],'XLim', [0 90], 'XTickLabels','');
-% hold on;
-% plot([0 90], [0 0], 'k-', 'LineWidth',0.3);
-% plot(PIFL.S.bins, PIFL.S.y, '-','LineWidth',1.2, 'Color',[55,126,184]/255);
-% % plot(PIFL.S.spl_bins, PIFL.S.spl, '-','LineWidth',1.2, 'Color',[55,126,184]/255);
-% text(8, 0.76, '\itPinus flexilis', 'FontSize',12, 'Color',[55,126,184]/255);
-% ylabel('S^{*} bias');
 
 axes(ax(2))
 fill([PSME.S.bins fliplr(PSME.S.bins)], [PSME.S.lci fliplr(PSME.S.uci)], [228,26,28]/255, 'FaceAlpha',0.4, 'EdgeColor','none')
@@ -333,7 +213,6 @@ set(gca, 'YLim',[-0.28 0.85],'XLim', [0 90]);
 hold on;
 plot([0 90], [0 0], 'k-', 'LineWidth',0.3);
 plot(PSME.S.bins, PSME.S.y, '-','LineWidth',1.2, 'Color',[228,26,28]/255);
-% plot(PSME.S.spl_bins, PSME.S.spl, '-','LineWidth',1.2, 'Color',[228,26,28]/255);
 text(8, 0.76, '\itPseudotsuga menziesii', 'FontSize',12, 'Color',[228,26,28]/255);
 ylabel('S^{*} bias');
 xlabel('D^{*} (cm)');
