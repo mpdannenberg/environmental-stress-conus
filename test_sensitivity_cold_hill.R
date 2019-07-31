4092f99522d9989bd630a418fdf4baf5d199a499
mpdannenberg <matthew-dannenberg@uiowa.edu>
2019-07-31 15:57:11 -0500

    Modify file names and locations

--- test_sensitivity_cold_hill.R
+++ test_sensitivity_cold_hill.R
@@ -1,9 +1,4 @@
-## Things to do for chapter 1
-#     Optimal growth using known diameter from PIPO sites
-#     Curve matching approach (Erika's idea) for estimating D bias
-#     Compare to flux tower GPP
-
-
+## Estimate sensitivity of A. rubrum and Quercus sp. to DBH uncertainty
 
 ## Load necessary functions and parameters
 library(dplR)
@@ -27,6 +22,46 @@ build.dbh.table = function(rwl, info){
     obs = info$dbh[ind]
     dbh.table$Est[i] = est
     dbh.table$Obs[i] = obs})
+  }
+  return(dbh.table)
+}
+
+build.dbh.table.lyf = function(rwl, info){
+  n = dim(rwl)[2]
+  dbh.table = data.frame(matrix(ncol=3, nrow=n))
+  names(dbh.table) <- c("Core", "Est", "Obs")
+  dbh.table$Core <- names(rwl)
+  for(i in 1:n){
+    x = rwl[,i]
+    est = sum(x[!is.na(x)]) # cm
+    name = dbh.table$Core[i]
+    site = substr(name, 1, 3)
+    tree = as.numeric(substr(name, 4, 5))
+    ind = which(info$Site %in% site & info$Tree==tree)
+    try({
+      obs = info$DBH[ind]
+      dbh.table$Est[i] = est
+      dbh.table$Obs[i] = obs})
+  }
+  return(dbh.table)
+}
+
+build.dbh.table.tow = function(rwl, info){
+  n = dim(rwl)[2]
+  dbh.table = data.frame(matrix(ncol=3, nrow=n))
+  names(dbh.table) <- c("Core", "Est", "Obs")
+  dbh.table$Core <- names(rwl)
+  for(i in 1:n){
+    x = rwl[,i]
+    est = sum(x[!is.na(x)]) # cm
+    name = dbh.table$Core[i]
+    site = substr(name, 1, 3)
+    tree = as.numeric(substr(name, 4, 6))
+    ind = which(info$Site %in% site & info$Tree==tree)
+    try({
+      obs = info$DBH[ind]
+      dbh.table$Est[i] = est
+      dbh.table$Obs[i] = obs})
   }
   return(dbh.table)
 }
@@ -75,17 +110,16 @@ comp.dopt = function(rwl, info, pars){
   return(list(d.naive, d.act, dopt.naive, dopt.act, rwl))
 }
 
-source("C:\\Users\\dannenberg\\Documents\\Publications\\Dannenberg_et_al_ForestEcolMgmt\\Sensitivity\\StressIndexFunctions.R")
+source("StressIndexFunctions.R")
 
-setwd("C:\\Users\\dannenberg\\Documents\\Data_Analysis\\Pederson_TreeRingData\\ColdHillPlateauKYTreeRingDataJan2015")
-info <- read.table('metadata.csv', header=T, sep=',')
+info <- read.table('./data/metadata.csv', header=T, sep=',')
 info$dbh = info$dbh * 2.54
 
-workDir = 'C:\\Users\\dannenberg\\Documents\\Publications\\Dannenberg_et_al_ForestEcolMgmt\\Sensitivity\\'
+info.lyf <- read.table('./data/lyf.field.data.csv', header=T, sep=',')
+info.tow <- read.table('./data/tow.field.data.csv', header=T, sep=',')
 
 
-
-##### QUVE #####
+##### QUVE & QUCO #####
 
 ## Optimal growth parameters
 pars <- vector(mode="list", length=5)
@@ -97,11 +131,11 @@ pars$b2 = NA
 pars$b3 = NA
 
 ## Read in tree-ring data
-trw = read.rwl('ColdHillBlackOakQUVE.rw') * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+trw = read.rwl('./data/ColdHillBlackOakQUVE.rw') * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
 
 ## Make data frame of estimated and observed DBH
 trw.dbh = build.dbh.table(trw, info)
-write.csv(trw.dbh, paste(c(workDir, "DBH_Comparison_QUVE.csv"), collapse=""))
+write.csv(trw.dbh, "./data/DBH_Comparison_QUVE.csv")
 
 ## Calculate actual and naive D and Dopt
 trw.comp.dopt = comp.dopt(trw, trw.dbh, pars)
@@ -128,15 +162,16 @@ rm(list = c("dD", "dDopt.naive", "dDopt.act", "D.naive", "D.act"))
 
 ## Write CSVs for use in MATLAB
 all.dopt.comp$d.naive.bins = cut(all.dopt.comp$d.naive, seq(from=0, to=150, by=10), include.lowest=TRUE)
+quve.dopt.comp = all.dopt.comp
 
-dopt.summary = boxplot(dopt.dif~d.naive.bins, data=all.dopt.comp, plot=FALSE)
-write.csv(cbind(dopt.summary$names, dopt.summary$n, dopt.summary$stats[3,]), paste(c(workDir, "Dopt_Comparison_QUVE.csv"), collapse=""))
+#dopt.summary = boxplot(dopt.dif~d.naive.bins, data=all.dopt.comp, plot=FALSE)
+#write.csv(cbind(dopt.summary$names, dopt.summary$n, dopt.summary$stats[3,]), paste(c(workDir, "Dopt_Comparison_QUVE.csv"), collapse=""))
 
-sD.summary = boxplot(s.dif.Dbased~d.naive.bins, data=all.dopt.comp, plot=FALSE)
-write.csv(cbind(sD.summary$names, sD.summary$n, sD.summary$stats[3,]), paste(c(workDir, "Stress_Comparison_QUVE.csv"), collapse=""))
+#sD.summary = boxplot(s.dif.Dbased~d.naive.bins, data=all.dopt.comp, plot=FALSE)
+#write.csv(cbind(sD.summary$names, sD.summary$n, sD.summary$stats[3,]), paste(c(workDir, "Stress_Comparison_QUVE.csv"), collapse=""))
 
-all.dopt.comp$d.naive.bins = NULL
-write.csv(all.dopt.comp, paste(c(workDir, "Sensitivity_QUVE.csv"), collapse=""))
+#all.dopt.comp$d.naive.bins = NULL
+#write.csv(all.dopt.comp, paste(c(workDir, "Sensitivity_QUVE.csv"), collapse=""))
 
 rm(list = c('all.dopt.comp', 'temp.comp.dopt', 'trw','trw.dbh','dopt.summary','pars','sD.summary','trw.comp.dopt'))
 
@@ -154,22 +189,48 @@ pars$b2 = NA
 pars$b3 = NA
 
 ## Read in tree-ring data
-trw = read.rwl('ColdHillEasternHemlockTSCA.rw') * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+trw.cold = read.rwl('./data/ColdHillEasternHemlockTSCA.rw') * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+trw.lyf = read.rwl('./data/LF_TSCA.rwl') * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+trw.tow = read.rwl('./data/TP_TSCA.rwl') * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
 
 ## Make data frame of estimated and observed DBH
-trw.dbh = build.dbh.table(trw, info)
-write.csv(trw.dbh, paste(c(workDir, "DBH_Comparison_TSCA.csv"), collapse=""))
+trw.dbh.cold = build.dbh.table(trw.cold, info)
+trw.dbh.lyf <- build.dbh.table.lyf(trw.lyf, info.lyf)
+trw.dbh.tow <- build.dbh.table.tow(trw.tow, info.tow)
+write.csv(rbind(trw.dbh.cold, trw.dbh.lyf, trw.dbh.tow), "./data/DBH_Comparison_TSCA.csv")
 
 ## Calculate actual and naive D and Dopt
-trw.comp.dopt = comp.dopt(trw, trw.dbh, pars)
-temp.comp.dopt = na.omit(cbind(c(apply(trw.comp.dopt[[1]], 2, rbind)), 
-                               c(apply(trw.comp.dopt[[2]], 2, rbind)), 
-                               c(apply(trw.comp.dopt[[3]], 2, rbind)), 
-                               c(apply(trw.comp.dopt[[4]], 2, rbind)), 
-                               c(apply(trw.comp.dopt[[5]], 2, rbind))))
-all.dopt.comp = data.frame(temp.comp.dopt)
-names(all.dopt.comp) = c("d.naive", "d.act", "dopt.naive", "dopt.act", "dD")
-all.dopt.comp$dopt.dif = all.dopt.comp$dopt.naive - all.dopt.comp$dopt.act
+trw.comp.dopt.cold = comp.dopt(trw.cold, trw.dbh.cold, pars)
+temp.comp.dopt.cold = na.omit(cbind(c(apply(trw.comp.dopt.cold[[1]], 2, rbind)), 
+                               c(apply(trw.comp.dopt.cold[[2]], 2, rbind)), 
+                               c(apply(trw.comp.dopt.cold[[3]], 2, rbind)), 
+                               c(apply(trw.comp.dopt.cold[[4]], 2, rbind)), 
+                               c(apply(trw.comp.dopt.cold[[5]], 2, rbind))))
+all.dopt.comp.cold = data.frame(temp.comp.dopt.cold)
+names(all.dopt.comp.cold) = c("d.naive", "d.act", "dopt.naive", "dopt.act", "dD")
+all.dopt.comp.cold$dopt.dif = all.dopt.comp.cold$dopt.naive - all.dopt.comp.cold$dopt.act
+
+trw.comp.dopt.lyf = comp.dopt(trw.lyf, trw.dbh.lyf, pars)
+temp.comp.dopt.lyf = na.omit(cbind(c(apply(trw.comp.dopt.lyf[[1]], 2, rbind)), 
+                                    c(apply(trw.comp.dopt.lyf[[2]], 2, rbind)), 
+                                    c(apply(trw.comp.dopt.lyf[[3]], 2, rbind)), 
+                                    c(apply(trw.comp.dopt.lyf[[4]], 2, rbind)), 
+                                    c(apply(trw.comp.dopt.lyf[[5]], 2, rbind))))
+all.dopt.comp.lyf = data.frame(temp.comp.dopt.lyf)
+names(all.dopt.comp.lyf) = c("d.naive", "d.act", "dopt.naive", "dopt.act", "dD")
+all.dopt.comp.lyf$dopt.dif = all.dopt.comp.lyf$dopt.naive - all.dopt.comp.lyf$dopt.act
+
+trw.comp.dopt.tow = comp.dopt(trw.tow, trw.dbh.tow, pars)
+temp.comp.dopt.tow = na.omit(cbind(c(apply(trw.comp.dopt.tow[[1]], 2, rbind)), 
+                                    c(apply(trw.comp.dopt.tow[[2]], 2, rbind)), 
+                                    c(apply(trw.comp.dopt.tow[[3]], 2, rbind)), 
+                                    c(apply(trw.comp.dopt.tow[[4]], 2, rbind)), 
+                                    c(apply(trw.comp.dopt.tow[[5]], 2, rbind))))
+all.dopt.comp.tow = data.frame(temp.comp.dopt.tow)
+names(all.dopt.comp.tow) = c("d.naive", "d.act", "dopt.naive", "dopt.act", "dD")
+all.dopt.comp.tow$dopt.dif = all.dopt.comp.tow$dopt.naive - all.dopt.comp.tow$dopt.act
+
+all.dopt.comp <- rbind(all.dopt.comp.cold, all.dopt.comp.lyf, all.dopt.comp.tow)
 
 ## Calculate Stress 
 dD = all.dopt.comp$dD
@@ -187,15 +248,15 @@ rm(list = c("dD", "dDopt.naive", "dDopt.act", "D.naive", "D.act"))
 all.dopt.comp$d.naive.bins = cut(all.dopt.comp$d.naive, seq(from=0, to=150, by=10), include.lowest=TRUE)
 
 dopt.summary = boxplot(dopt.dif~d.naive.bins, data=all.dopt.comp, plot=FALSE)
-write.csv(cbind(dopt.summary$names, dopt.summary$n, dopt.summary$stats[3,]), paste(c(workDir, "Dopt_Comparison_TSCA.csv"), collapse=""))
+write.csv(cbind(dopt.summary$names, dopt.summary$n, dopt.summary$stats[3,]), "./data/Dopt_Comparison_TSCA.csv")
 
 sD.summary = boxplot(s.dif.Dbased~d.naive.bins, data=all.dopt.comp, plot=FALSE)
-write.csv(cbind(sD.summary$names, sD.summary$n, sD.summary$stats[3,]), paste(c(workDir, "Stress_Comparison_TSCA.csv"), collapse=""))
+write.csv(cbind(sD.summary$names, sD.summary$n, sD.summary$stats[3,]), "./data/Stress_Comparison_TSCA.csv")
 
 all.dopt.comp$d.naive.bins = NULL
-write.csv(all.dopt.comp, paste(c(workDir, "Sensitivity_TSCA.csv"), collapse=""))
+write.csv(all.dopt.comp, "./data/Sensitivity_TSCA.csv")
 
-rm(list = c('all.dopt.comp', 'temp.comp.dopt', 'trw','trw.dbh','dopt.summary','pars','sD.summary','trw.comp.dopt'))
+rm(list = c(ls(pattern='^all'), ls(pattern='^temp'), ls(pattern='^trw'), 'dopt.summary','pars','sD.summary'))
 
 
 
@@ -213,7 +274,7 @@ pars$b2 = NA
 pars$b3 = NA
 
 ## Read in tree-ring data
-trw = read.rwl('ColdHillScarletOakQUCO.rw') * 0.1 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+trw = read.rwl('./data/ColdHillScarletOakQUCO.rw') * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
 
 ## Make data frame of estimated and observed DBH
 trw.dbh = build.dbh.table(trw, info)
@@ -327,7 +388,7 @@ pars$b2 = NA
 pars$b3 = NA
 
 ## Read in tree-ring data
-trw = read.rwl('ColdHillRedMapleACRU.rw') * 0.1 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+#trw = read.rwl('./data/ColdHillRedMapleACRU.rw') * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
 
 ## Make data frame of estimated and observed DBH
 trw.dbh = build.dbh.table(trw, info)
