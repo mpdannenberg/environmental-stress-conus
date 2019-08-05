ab829f0c7ce46b03ffd6d6437d98a55238a65957
mpdannenberg <matthew-dannenberg@uiowa.edu>
2019-08-05 12:37:18 -0500

    Change file locations and remove PIFL

--- test_sensitivity_pipo_psme.R
+++ test_sensitivity_pipo_psme.R
@@ -1,9 +1,4 @@
-## Things to do for chapter 1
-#     Optimal growth using known diameter from PIPO sites
-#     Curve matching approach (Erika's idea) for estimating D bias
-#     Compare to flux tower GPP
-
-
+## Estimate sensitivity of P. ponderosa and P. menziesii to DBH uncertainty
 
 ## Load necessary functions and parameters
 library(dplR)
@@ -72,7 +67,7 @@ comp.dopt = function(rwl, info, pars){
   return(list(d.naive, d.act, dopt.naive, dopt.act, rwl))
 }
 
-source("C:\\Users\\C-TRES\\Documents\\Matt\\Code\\StressIndexFunctions.R")
+source("StressIndexFunctions.R")
 
 
 #####  PIPO  #####
@@ -86,21 +81,20 @@ pars$Hmax = 7800
 pars$b2 = NA
 pars$b3 = NA
 
-setwd("C:\\Users\\C-TRES\\OneDrive - University of North Carolina at Chapel Hill\\Chpt2\\Sensitivity\\PIPO")
-bcr <- read.rwl("BCR_QCd_040715.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
-fod <- read.rwl("FOD_QCd_032315.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+bcr <- read.rwl("./data/BCR_QCd_040715.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+fod <- read.rwl("./data/FOD_QCd_032315.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
 fod$FOD041C1 = NULL
 fod$FOD041C2 = NULL
 fod$FOD033H1 = NULL
 fod$FOD033H2 = NULL
-rrr <- read.rwl("RRR_QCd_041315.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+rrr <- read.rwl("./data/RRR_QCd_041315.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
 rrr$RRR019E1 = NULL
 rrr$RRR019E2 = NULL
-snd <- read.rwl("SND_QCd_040915.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+snd <- read.rwl("./data/SND_QCd_040915.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
 snd$SND033E1 = NULL
 snd$SND033E2 = NULL
-sug <- read.rwl("SUG_QCd_021615.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
-tmr <- read.rwl("TMR_QCd_043015.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+sug <- read.rwl("./data/SUG_QCd_021615.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+tmr <- read.rwl("./data/TMR_QCd_043015.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
 tmr$TMRX01 = NULL
 tmr$TMR024D1 = NULL
 tmr$TMR024D2 = NULL
@@ -125,7 +119,7 @@ snd.dbh = build.dbh.table(snd, snd.info)
 sug.dbh = build.dbh.table(sug, sug.info)
 tmr.dbh = build.dbh.table(tmr, tmr.info)
 all.dbh = rbind(bcr.dbh, fod.dbh, rrr.dbh, snd.dbh, sug.dbh, tmr.dbh)
-write.csv(all.dbh, 'DBH_Comparison_PIPO.csv')
+write.csv(all.dbh, './data/DBH_Comparison_PIPO.csv')
 
 
 ## Calculate actual and naive D and Dopt
@@ -167,121 +161,15 @@ rm(list = c("dD", "dDopt.naive", "dDopt.act", "D.naive", "D.act"))
 all.dopt.comp$d.naive.bins = cut(all.dopt.comp$d.naive, seq(from=0, to=150, by=10), include.lowest=TRUE)
 
 dopt.summary = boxplot(dopt.dif~d.naive.bins, data=all.dopt.comp, plot=FALSE)
-write.csv(cbind(dopt.summary$names, dopt.summary$n, dopt.summary$stats[3,]), 'Dopt_Comparison_PIPO.csv')
+write.csv(cbind(dopt.summary$names, dopt.summary$n, dopt.summary$stats[3,]), './data/Dopt_Comparison_PIPO.csv')
 
 sD.summary = boxplot(s.dif.Dbased~d.naive.bins, data=all.dopt.comp, plot=FALSE)
-write.csv(cbind(sD.summary$names, sD.summary$n, sD.summary$stats[3,]), 'Stress_Comparison_PIPO.csv')
+write.csv(cbind(sD.summary$names, sD.summary$n, sD.summary$stats[3,]), './data/Stress_Comparison_PIPO.csv')
 
 all.dopt.comp$d.naive.bins = NULL
-write.csv(all.dopt.comp, 'Sensitivity_PIPO.csv')
+write.csv(all.dopt.comp, './data/Sensitivity_PIPO.csv')
 
 rm(list = setdiff(ls(), lsf.str()))
-
-
-
-#####  PIFL  #####
-
-## Optimal growth parameters for PIFL
-pars <- vector(mode="list", length=5)
-names(pars) <- c("G", "Dmax", "Hmax", "b2", "b3")
-pars$G = 14.1
-pars$Dmax = 230
-pars$Hmax = 2590
-pars$b2 = NA
-pars$b3 = NA
-
-setwd("C:\\Users\\C-TRES\\OneDrive - University of North Carolina at Chapel Hill\\Chpt2\\Sensitivity\\PIFL")
-bwc <- read.rwl("BWC.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
-cwc <- read.rwl("CWC_PIFL.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
-cwc[, nchar(names(cwc))>6] = NULL # Get rid of multi-part cores
-cwc$CWC01A = NULL
-cwc$CWC01B = NULL
-cwc$CWC02A = NULL
-cwc$CWC04A = NULL
-cwc$CWC04B = NULL
-rdh <- read.rwl("RDH.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
-rdh[, nchar(names(rdh))>6] = NULL # Get rid of multi-part cores
-rdh$RDH01B = NULL
-rdh$RDH02A = NULL
-rdh$RDH02B = NULL
-rdh$RDH03A = NULL
-rdh$RDH03B = NULL
-rdh$RDH06A = NULL
-rdh$RDH06B = NULL
-rdh$RDH42C = NULL # two different DBH measurements
-rdh$RDH28A = NULL # double-stem (not sure which core from which stem)
-rdh$RDH35A = NULL # quad-stem (not sure which core from which stem)
-rdh$RDH35B = NULL # quad-stem (not sure which core from which stem)
-rdh$RDH35C = NULL # quad-stem (not sure which core from which stem)
-wgh <- read.rwl("WGH.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
-wgh[, nchar(names(wgh))>6] = NULL # Get rid of multi-part cores
-wgh$WGH39B = NULL # two different DBH measurements
-wgh$WGH39C = NULL # two different DBH measurements
-wgh$WGH28B = NULL # cores coming from multiple parts of tree (don't use)
-wgh$WGH21D = NULL # measured DBH only applies to A-C
-
-
-## Get measured DBH
-bwc.info <- read.table("BWC_info.csv", sep=",", header=T, na.strings=-9999)
-cwc.info <- read.table("CWC_info.csv", sep=",", header=T, na.strings=-9999)
-rdh.info <- read.table("RDH_info.csv", sep=",", header=T, na.strings=-9999)
-wgh.info <- read.table("WGH_info.csv", sep=",", header=T, na.strings=-9999)
-
-
-## Make data frame of estimated and observed DBH
-bwc.dbh = build.dbh.table(bwc, bwc.info)
-cwc.dbh = build.dbh.table(cwc, cwc.info)
-rdh.dbh = build.dbh.table(rdh, rdh.info)
-wgh.dbh = build.dbh.table(wgh, wgh.info)
-all.dbh = rbind(bwc.dbh, cwc.dbh, rdh.dbh, wgh.dbh)
-write.csv(all.dbh, 'DBH_Comparison_PIFL.csv')
-
-
-## Calculate actual and naive D and Dopt
-bwc.all = comp.dopt(bwc, bwc.dbh, pars)
-cwc.all = comp.dopt(cwc, cwc.dbh, pars)
-rdh.all = comp.dopt(rdh, rdh.dbh, pars)
-wgh.all = comp.dopt(wgh, wgh.dbh, pars)
-
-
-## Reshape into long form
-temp.bwc = na.omit(cbind(c(apply(bwc.all[[1]], 2, rbind)), c(apply(bwc.all[[2]], 2, rbind)), c(apply(bwc.all[[3]], 2, rbind)), c(apply(bwc.all[[4]], 2, rbind)), c(apply(bwc.all[[5]], 2, rbind))))
-temp.cwc = na.omit(cbind(c(apply(cwc.all[[1]], 2, rbind)), c(apply(cwc.all[[2]], 2, rbind)), c(apply(cwc.all[[3]], 2, rbind)), c(apply(cwc.all[[4]], 2, rbind)), c(apply(cwc.all[[5]], 2, rbind))))
-temp.rdh = na.omit(cbind(c(apply(rdh.all[[1]], 2, rbind)), c(apply(rdh.all[[2]], 2, rbind)), c(apply(rdh.all[[3]], 2, rbind)), c(apply(rdh.all[[4]], 2, rbind)), c(apply(rdh.all[[5]], 2, rbind))))
-temp.wgh = na.omit(cbind(c(apply(wgh.all[[1]], 2, rbind)), c(apply(wgh.all[[2]], 2, rbind)), c(apply(wgh.all[[3]], 2, rbind)), c(apply(wgh.all[[4]], 2, rbind)), c(apply(wgh.all[[5]], 2, rbind))))
-
-all.dopt.comp = data.frame(rbind(temp.bwc, temp.cwc, temp.rdh, temp.wgh))
-names(all.dopt.comp) = c("d.naive", "d.act", "dopt.naive", "dopt.act", "dD")
-all.dopt.comp$dopt.dif = all.dopt.comp$dopt.naive - all.dopt.comp$dopt.act
-
-
-## Calculate Stress 
-dD = all.dopt.comp$dD
-dDopt.naive = all.dopt.comp$dopt.naive
-dDopt.act = all.dopt.comp$dopt.act
-D.naive = all.dopt.comp$d.naive
-D.act = all.dopt.comp$d.act
-
-all.dopt.comp$s.naive.Dbased = all.dopt.comp$dD / all.dopt.comp$dopt.naive
-all.dopt.comp$s.act.Dbased = all.dopt.comp$dD / all.dopt.comp$dopt.act
-all.dopt.comp$s.dif.Dbased = all.dopt.comp$s.naive.Dbased - all.dopt.comp$s.act.Dbased
-rm(list = c("dD", "dDopt.naive", "dDopt.act", "D.naive", "D.act"))
-
-
-## Write CSVs for use in MATLAB
-all.dopt.comp$d.naive.bins = cut(all.dopt.comp$d.naive, seq(from=0, to=150, by=10), include.lowest=TRUE)
-
-dopt.summary = boxplot(dopt.dif~d.naive.bins, data=all.dopt.comp, plot=FALSE)
-write.csv(cbind(dopt.summary$names, dopt.summary$n, dopt.summary$stats[3,]), 'Dopt_Comparison_PIFL.csv')
-
-sD.summary = boxplot(s.dif.Dbased~d.naive.bins, data=all.dopt.comp, plot=FALSE)
-write.csv(cbind(sD.summary$names, sD.summary$n, sD.summary$stats[3,]), 'Stress_Comparison_PIFL.csv')
-
-all.dopt.comp$d.naive.bins = NULL
-write.csv(all.dopt.comp, 'Sensitivity_PIFL.csv')
-
-rm(list = setdiff(ls(), lsf.str()))
-
 
 
 #####  PSME  #####
@@ -295,14 +183,13 @@ pars$Hmax = 8500
 pars$b2 = NA
 pars$b3 = NA
 
-setwd("C:\\Users\\C-TRES\\OneDrive - University of North Carolina at Chapel Hill\\Chpt2\\Sensitivity\\PSME")
-cwc <- read.rwl("CWC_PSME.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+cwc <- read.rwl("./data/CWC_PSME.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
 cwc[, nchar(names(cwc))>6] = NULL # Get rid of multi-part cores
 cwc$CWC23A = NULL # no DBH recorded
-efv <- read.rwl("EFV_PSME.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+efv <- read.rwl("./data/EFV_PSME.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
 efv[, nchar(names(efv))>6] = NULL # Get rid of multi-part cores
 efv$EFV12A = NULL # no DBH recorded
-etc <- read.rwl("ETC_PSME.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
+etc <- read.rwl("./data/ETC_PSME.txt") * 0.1 * 2 #Multiply by 2 (radial growth --> diameter growth) and 0.1 (mm --> cm)
 etc[, nchar(names(etc))>6] = NULL # Get rid of multi-part cores
 etc$ETC01A = NULL
 etc$ETC01B = NULL
@@ -328,7 +215,7 @@ cwc.dbh = build.dbh.table(cwc, cwc.info)
 efv.dbh = build.dbh.table(efv, efv.info)
 etc.dbh = build.dbh.table(etc, etc.info)
 all.dbh = rbind(cwc.dbh, efv.dbh, etc.dbh)
-write.csv(all.dbh, 'DBH_Comparison_PSME.csv')
+write.csv(all.dbh, './data/DBH_Comparison_PSME.csv')
 
 
 ## Calculate actual and naive D and Dopt
@@ -366,22 +253,14 @@ rm(list = c("dD", "dDopt.naive", "dDopt.act", "D.naive", "D.act"))
 all.dopt.comp$d.naive.bins = cut(all.dopt.comp$d.naive, seq(from=0, to=150, by=10), include.lowest=TRUE)
 
 dopt.summary = boxplot(dopt.dif~d.naive.bins, data=all.dopt.comp, plot=FALSE)
-write.csv(cbind(dopt.summary$names, dopt.summary$n, dopt.summary$stats[3,]), 'Dopt_Comparison_PSME.csv')
+write.csv(cbind(dopt.summary$names, dopt.summary$n, dopt.summary$stats[3,]), './data/Dopt_Comparison_PSME.csv')
 
 sD.summary = boxplot(s.dif.Dbased~d.naive.bins, data=all.dopt.comp, plot=FALSE)
-write.csv(cbind(sD.summary$names, sD.summary$n, sD.summary$stats[3,]), 'Stress_Comparison_PSME.csv')
+write.csv(cbind(sD.summary$names, sD.summary$n, sD.summary$stats[3,]), './data/Stress_Comparison_PSME.csv')
 
 all.dopt.comp$d.naive.bins = NULL
-write.csv(all.dopt.comp, 'Sensitivity_PSME.csv')
+write.csv(all.dopt.comp, './data/Sensitivity_PSME.csv')
 
 rm(list = setdiff(ls(), lsf.str()))
-
-
-
-
-
-
-
-
 
 
